bdafa39843272264619d3472c4abf4fe54a6e7b1
Ieva <ievoms@gmail.com>
2019-02-27 20:23:06 +0200

    veikia autocomplete, bet ne dropdown menu

--- src/components/autocomplete/autocomplete.js
+++ src/components/autocomplete/autocomplete.js
@@ -17,52 +17,55 @@ export default class Autocomplete extends React.Component {
       //loading: false,
       // movies: [],
       // text: '',
-      suggestions: []
+      suggestions: [],
+      pasiulymai: []
     };
   }
 
   componentDidMount() {
 
     const url = 'https://api.themoviedb.org/3/search/movie?api_key=cab2afe8b43cf5386e374c47aeef4fca&language=en-US&query=movie&page=1&include_adult=false'
-    fetch(url).then(resp => resp.json())
+    fetch(url)
+    .then(resp => resp.json())
       .then(data => {
-        let title = data.resp.map((item) => {
-          return (
-            <div key={item.title}>
-              {item.title}
-            </div>
-          )
-        })
-        this.setState({ suggestions: title })
-        console.log("state", this.state.suggestions)
+        console.log(data.results)
+        // let title = data.map(item => {
+        //   return (
+        //     <div key={item.title}>
+        //       {item.title}
+        //     </div>
+        //   )
+        // })
+        this.setState({ suggestions: data.results })
+        // console.log("state", this.state.suggestions)
       })
   }
 
 
   onTextChanged = (e) => {
-    console.log("on Text veikia");
+    const { suggestions } = this.state;
     const value = e.target.value;
     let pasiulymai = [];
 
-    if (value.length > 0) {
+    if (value.length >= 3) {
       const regex = new RegExp(event.target.value, 'i');
-      console.log("regex value: ", regex.value);
-      pasiulymai = this.suggestions.sort().filter(v => regex.test(v));
+      pasiulymai = suggestions.filter(v => regex.test(v.title));
     }
-    this.setState(() => ({suggestions: pasiulymai }));
-    console.log(this.state.pasiulymai);
+    this.setState({pasiulymai: pasiulymai });
+    console.log(suggestions);
+    console.log(pasiulymai);
   }
 
 
   
     renderSuggestions() {
-      const { suggestions } = this.state;
-      if (suggestions.length === 0) {
+      const { pasiulymai } = this.state;
+      if (pasiulymai.length === 0) {
         return null;
       }
       return (
         <ul>
-         {this.suggestions.map((item )=> <li key={item.title}>{item.title}</li>)}
+         {pasiulymai.map((item )=> <li key={item.title}>{item.title}</li>)}
         </ul>
       );
   
@@ -75,21 +78,22 @@ export default class Autocomplete extends React.Component {
       <div className={styles.container}>
 
         <div className ={styles.InputWithIcon}>
-        <label className ={styles.label} for="filmas" >
-        {"Enter movie name"}
+        <label className ={styles.label} htmlFor="filmas" >
+      
         </label>
           <input onChange={this.onTextChanged} className={styles.input} id="filmas" placeholder=" Enter movie name" autoComplete="on" />
-          <i className={"fa fa-film"} aria-hidden ="true"></i>
+          <i className={"fa fa-film"} ></i>
         </div>
         <button type="sFbmit" className={styles.button}>
           <i className={"fa fa-search"}></i>
         </button>
         {//this.items.map((item) => <li key={item.title}>{item}</li>)
-         // this.renderSuggestions()}
+         this.renderSuggestions()
         // this.state.suggestions
         }
 
-        <p>{ this.state.suggestions }</p>
+        {//<p>{ this.state.suggestions }</p>
+      }
       </div>
 
     )
