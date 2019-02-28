fec10991a5f6cba21f5492fc93b3b9cf7612c06e
Ieva <ievoms@gmail.com>
2019-02-28 16:12:12 +0200

    Galutinis

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,26 +1,13 @@
 import { hot } from 'react-hot-loader/root';
-
 import React from 'react';
-
 import Autocomplete from '../autocomplete';
-//import Fetch from './fetching';
-import Suggestions from '../app/suggestions';
-
 import styles from './app.css';
 
-
 const App = () => (
-  
     <div className={styles.container}>
-    
-    <Autocomplete />
-    
-    {//<Fetch/>
-    }
-        </div>
-   
+        <Autocomplete />
+    </div>
+
 );
-
-
 
 export default hot(App);
--- src/components/autocomplete/autocomplete.js
+++ src/components/autocomplete/autocomplete.js
@@ -1,22 +1,11 @@
 import React from 'react';
-
-//import Fetch from '../app/fetching';
-
 import styles from './autocomplete.css';
-//import suggestions from '../app/suggestions';
-//import Card from './../app/card';
-//import Suggestions from './../app/suggestions';
-
-
 
 export default class Autocomplete extends React.Component {
   constructor() {
     super();
-   
+
     this.state = {
-      //loading: false,
-      // movies: [],
-      // text: '',
       suggestions: [],
       pasiulymai: []
     };
@@ -26,18 +15,9 @@ export default class Autocomplete extends React.Component {
 
     const url = 'https://api.themoviedb.org/3/search/movie?api_key=cab2afe8b43cf5386e374c47aeef4fca&language=en-US&query=movie&page=1&include_adult=false'
     fetch(url)
-    .then(resp => resp.json())
+      .then(resp => resp.json())
       .then(data => {
-        console.log(data.results)
-        // let title = data.map(item => {
-        //   return (
-        //     <div key={item.title}>
-        //       {item.title}
-        //     </div>
-        //   )
-        // })
         this.setState({ suggestions: data.results })
-        // console.log("state", this.state.suggestions)
       })
   }
 
@@ -46,57 +26,42 @@ export default class Autocomplete extends React.Component {
     const { suggestions } = this.state;
     const value = e.target.value;
     let pasiulymai = [];
-
     if (value.length >= 3) {
       const regex = new RegExp(event.target.value, 'i');
       pasiulymai = suggestions.filter(v => regex.test(v.title));
     }
-    this.setState({pasiulymai: pasiulymai });
-    console.log(suggestions);
-    console.log(pasiulymai);
+    this.setState({ pasiulymai: pasiulymai });
   }
 
-
-  
-    renderSuggestions() {
-      const { pasiulymai } = this.state;
-      if (pasiulymai.length === 0) {
-        return null;
-      }
-      return (
-        <ul>
-         {pasiulymai.map((item )=> <li key={item.title}>{item.title}</li>)}
-        </ul>
-      );
-  
+  renderSuggestions() {
+    const { pasiulymai } = this.state;
+    if (pasiulymai.length === 0) {
+      return null;
     }
-  
+    return (
+      <div className={styles.list}>
+        {pasiulymai.map((item) => <p key={item.title}>{item.title}</p>)}
+      </div>
+    );
+
+  }
+
   render() {
-    // const { text } = this.state;
     return (
 
       <div className={styles.container}>
 
-        <div className ={styles.InputWithIcon}>
-        <label className ={styles.label} htmlFor="filmas" >
-      
-        </label>
+        <div className={styles.InputWithIcon}>
           <input onChange={this.onTextChanged} className={styles.input} id="filmas" placeholder=" Enter movie name" autoComplete="on" />
           <i className={"fa fa-film"} ></i>
         </div>
         <button type="sFbmit" className={styles.button}>
           <i className={"fa fa-search"}></i>
         </button>
-        {//this.items.map((item) => <li key={item.title}>{item}</li>)
-         this.renderSuggestions()
-        // this.state.suggestions
-        }
+        {this.renderSuggestions()}
 
-        {//<p>{ this.state.suggestions }</p>
-      }
       </div>
 
     )
   }
-
 }
